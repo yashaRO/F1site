df797e1e66bfd8ab09ce18cd8d669bdda8e7846e
yashaRO <yashanyou@gmail.com>
2016-10-21 21:25:57 -0500

    Finished BlackJack Game

--- Blackjack/SBJ.js
+++ Blackjack/SBJ.js
@@ -1,9 +1,9 @@
 var gScore = 100;
 var rScore = 0;
-var grades = {a:0,b:0,c:0,d:0,f:0}
+var grades = [0,0,0,0,0]
 var round = ['Round 1','Round 2', 'Round 3', 'Round 4', 'Round 5']
 var i = 0;
-var gamestart = 0;
+var standing = 0;
 var cards = function() {
 	return [{cardv:1,link:'cards/ace_of_clubs.png'},
 	{cardv:1,link:'cards/ace_of_diamonds.png'},
@@ -59,11 +59,8 @@ var cards = function() {
 	{cardv:10,link:'cards/king_of_spades.png'}]
 	}
 var cardcopy = cards();
-	var hit = function() {
-	if (rScore >= 21) {
-		return;
-	}
-	gamestart = 1;
+function hit() {
+	standing = 1;
 	document.getElementById('round').innerHTML = round[i]
 	var card = Math.floor(Math.random()*cardcopy.length);
 	rScore += cardcopy[card].cardv;
@@ -71,21 +68,21 @@ var cardcopy = cards();
 	cardcopy.splice(card,1);
 	document.getElementById('rScore').innerHTML = rScore;
 	if (rScore >= 21) {
+		document.getElementById('demo2').innerHTML = '&nbsp';
 		if (rScore > 21) {
 			rScore = 0;
 			document.getElementById('demo1').innerHTML = 'BUST!';
-			document.getElementById('demo2').innerHTML = '&nbsp';
 			stand();
 			return;
 		}
 		document.getElementById('demo1').innerHTML = 'BLACKJACK';
-		document.getElementById('demo2').innerHTML = '&nbsp';
 		stand();
 		return;
 	}
 }
 function stand() {
-	if (gamestart == 0) {return}
+	if (standing == 0) {return}
+	standing = 0;
 	document.getElementById('hit').innerHTML = 'Continue';
 	document.getElementById('hit').onclick = cont;
 	document.getElementById('gScore').innerHTML = gScore -= rScore;
@@ -93,7 +90,8 @@ function stand() {
 	cardcopy = cards();
 	if (i == 4) {
 		document.getElementById('hit').innerHTML = "Play Again?";
-		reset()
+		document.getElementById('hit').onclick = reset;
+		grade(gScore);
 	}
 	else {i++;}
 }
@@ -114,4 +112,30 @@ function reset() {
 	document.getElementById('gScore').innerHTML = 100;
 	document.getElementById('rScore').innerHTML = 0;
 	$('.card').empty();
+}
+function giveGrade() {
+	document.getElementById('demo1').innerHTML = 'You have';
+	document.getElementById('demo2').innerHTML = "A: " + grades[0] + " B: " +  grades[1] + " C: " + grades[2] + " D: " + grades[3] + " F: " + grades[4];
+}
+function grade(number) {
+	if (number > 39) {
+		grades[4]++
+		alert("You got an F! Trash :/")
+	}
+	else if (number > 29) {
+		grades[3]++
+		alert("You got an D! Try Harder")
+	}
+	else if (number > 19) {
+		grades[2]++
+		alert("You got an C! Close!")
+	}
+	else if (number > 9) {
+		grades[1]++
+		alert("You got an B! Almost perfect!")
+	}
+	else {
+		grades[0]++
+		alert("You got an A! YAAAAAAAS!")
+	}
 }
