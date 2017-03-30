3b9dccf50da5d4704db36d9918f3cc66be185d05
yashaRO <yashanyou@gmail.com>
2017-03-30 18:53:08 -0500

    Rehaul, old stuff will be used later as a 'previous project'

--- Base.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$('th').mouseenter(function() {
-		$(this).fadeTo('fast',.7)
-	});
-	$('th').mouseleave(function() {
-		$(this).fadeTo('fast',1)
-	});
-})
---
+++ assets/js/frontpage.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		normalScrollElements: '#bioText'
+	});
+});
